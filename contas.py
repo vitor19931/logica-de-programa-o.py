9b9db1a1fc62f3025ea4ca3789e5ba13b3eea231
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:36:47 -0300

    feat: tipagem e contas

---
+++ contas.py
@@ -0,0 +1,22 @@
+import os
+os.system("cls")
+
+numero_1 = float(input("digite o numero_1: "))
+numero_2 = float(input("digite o numero_2: "))
+
+
+
+
+
+soma = numero_1 + numero_2
+subtracao = numero_1 - numero_2 
+multiplicacao = numero_1 * numero_2 
+divisao = numero_1 / numero_2
+
+print(f"Resultado da soma: {soma} ")
+
+print(f"Resultado da subtracao: {subtracao} ")
+
+print(f"Resultado da multiplicacao: {multiplicacao} ")
+
+print(f"Resultado da divisao: {divisao} ")
---
+++ tipagem.py
@@ -0,0 +1,13 @@
+# Tipagem dinâmica
+
+
+import os
+os.system
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
